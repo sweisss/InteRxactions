f95436dd0bb31c2ad2e62fcd2112b4abb6ac5ea6
Seth Weiss <sweisss@gmail.com>
2026-02-20 21:09:13 -0800

    Add drug type to actual API query

--- app/src/main/java/com/example/interxactions/ui/InteractingDrugsListFragment.kt
+++ app/src/main/java/com/example/interxactions/ui/InteractingDrugsListFragment.kt
@@ -99,6 +99,7 @@ class InteractingDrugsListFragment : Fragment(R.layout.interacting_drugs_list_fr
 
     // The name of the drug that was searched
     private lateinit var searchedDrugName: String
+    private lateinit var searchedDrugType: String
 
     private lateinit var displayListCache: List<DrugInteractionsDisplay>
 
@@ -143,10 +144,11 @@ class InteractingDrugsListFragment : Fragment(R.layout.interacting_drugs_list_fr
 
             // Set up the RecyclerView and buttons
             searchedDrugName = drug.drugName // Get the name of the drug that was searched
+            searchedDrugType = drug.drugType
             drugNameTitle.text = getString(R.string.search_results_title, searchedDrugName) // Set the title of the drug that was searched
             Log.d("InteractingDrugsListFragment", "Searched drug outside observe: $searchedDrugName")
 
-            val query = "drug_interactions:$searchedDrugName AND _exists_:openfda.generic_name" // Create the query to search for drug interactions
+            val query = "drug_interactions:$searchedDrugName AND _exists_:openfda.${searchedDrugType}" // Create the query to search for drug interactions
             Log.d("InteractingDrugsListFragment", "Query: $query")
 
             viewModel.loadDrugInteractions(query) // Load the drug interactions for the given search query
